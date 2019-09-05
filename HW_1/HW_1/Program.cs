5580c599aabf9ae6e1dde747e10365731c3c98b4
alex <alex.killian.13@hotmail.com>
2019-09-05 17:51:46 -0600

    drafted homework 1

---
+++ HW_1/HW_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setting variables up
+
+            int age = 0;
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 10);
+            
+            // establishing name
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            //establish age
+            double accurateAge = age;
+            accurateAge += 23;
+    
+            Console.WriteLine("Oh nice to meet you " + name + " my name is alex! How old are you?");
+            age = Int32.Parse(Console.ReadLine());
+            if(age < 20)
+            {
+                Console.WriteLine(age + " is the perfect age, still so much time to acheive great things.");
+            }
+            else if(age > 20)
+            {
+                Console.WriteLine("Youre getting old like me!");
+            }
+
+            
+            Console.WriteLine("If we add our ages together we would be " + (age + accurateAge) + "! What is your favorite game?");
+            string game = Console.ReadLine();
+
+            Console.WriteLine("I also enjoy " + game + " , but if i had to choose, i think League of Legends would be my favorite!");
+            Console.WriteLine("Lets play a little math game with your age! Answer the questions in order.");
+
+            //setting up array for math game
+            int[] myNumbers = new int[10];
+           
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                myNumbers[i] = i + randomNumber;
+            }
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                Console.WriteLine("What is " + age + "+" + myNumbers[i]);
+            }
+
+            //user answer
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+            //get actual answer
+            int actualAnswer = age + myNumbers[0]; 
+            //compare answers
+            if(userAnswer == actualAnswer)
+            {
+                Console.WriteLine("good job!");
+            }
+            else
+            {
+                Console.WriteLine("That is not correct");
+            }
+            Console.ReadLine();
+        }
+    }
+}
