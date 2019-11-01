50cc1205c64b80e68218bae54eed76f7204ede84
alex <alex.killian.13@hotmail.com>
2019-10-31 21:13:57 -0600

    Started Basics of 2d game

---
+++ Rogue Runner/Assets/Scripts/Feet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feet : MonoBehaviour
+{
+    PlayerCtrl player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerCtrl>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Ground") && player.isJumping)
+        {
+            player.isJumping = false;
+        }
+    }
+
+}
---
+++ Rogue Runner/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float playerSpeed;
+    private float move;
+
+    public float jumpSpeed;
+    public bool isJumping;
+    
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * playerSpeed, rb.velocity.y);
+
+        if (move < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+
+        }
+        else if (move > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed));
+            isJumping = true;
+        }
+
+
+
+
+        RunAnimations();
+
+
+    }
+
+
+    void RunAnimations()
+    {
+        anim.SetFloat("Movement", Mathf.Abs(move));
+        anim.SetBool("isJumping", isJumping);
+    }
+
+}
