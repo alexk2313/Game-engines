0df5f8bf28ab22d31160fcbc6184aa15ff1afc2a
alex <alex.killian.13@hotmail.com>
2019-11-13 00:28:14 -0700

    Started Town Level

---
+++ Rogue Runner/Assets/Scripts/DoorTransition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTransition : MonoBehaviour
+{
+    [SerializeField]
+    private string levelToLoad;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("collision");
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
---
+++ Rogue Runner/Assets/Scripts/TransitionTimer.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 30f;
+
+    
+    private float timer;
+ 
+
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if(timer > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+          
+    }
+}
